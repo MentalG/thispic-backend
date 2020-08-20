35958011d6da8fe3a4633c67916576f0a4523e09
MentalG <gromovthefirst@gmail.com>
2020-08-20 22:58:14 +0300

    add: post request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const postsRoutes = require('./src/routes/posts')
+const bodyParser = require('body-parser')
 
 const app = express();
 const DB_CONNECTION = process.env.DB_CONNECTION;
@@ -9,9 +10,9 @@ const connectionConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 
 mongoose.connect(DB_CONNECTION, connectionConfig, () => console.log('Connected'));
 
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.send('We are on home')
 })
 app.use('/posts', postsRoutes);
-
 app.listen(3000);
