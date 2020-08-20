8b0a55ab39bb56b913a76a10ce9b0b2508f4cd14
MentalG <gromovthefirst@gmail.com>
2020-08-20 16:26:39 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const pixel = require('pixel');
+const COLORS = require('./colors');
+const countBy = require('lodash.countby');
+
+const getImageData = async () => {
+    const image = fs.readFileSync('./materials/test.jpg');
+    const imageData = await pixel(image);
+
+    return imageData[0].data
+}
+
+const getPixelColors = (data, pixels) => {
+  const sortedColors = [];
+
+  for (let i = 0; i < pixels; i++) {
+    let result = data.slice(i * 4, i * 4 + 3);
+    sortedColors.push(result);
+  }
+
+  return sortedColors;
+}
+
+const getClosestColor = (pixelsColors) => {
+  const closestColors = [];
+
+  pixelsColors.map((color) => {
+    const result = rgbToHex(...color)
+    for (let key in COLORS) {
+      COLORS[key].map((item) => {
+        if(item === result) return closestColors.push(key)
+      })
+    }
+  });
+
+  return closestColors;
+}
+
+const rgbToHex = (r, g, b) => {
+  return '' + numberToHex(r) + numberToHex(g) + numberToHex(b);
+};
+
+const numberToHex = (number) => {
+  const hex = roundColor(number).toString(16);
+  return hex.length == 1 ? '0' + hex : hex;
+};
+
+const roundColor = (color) => {
+  return Math.round(color / 51) * 51;
+};
+
+const getCountOfColors = async () => {
+  try {
+    const data = await getImageData();
+    const pixels = data.length / 4;
+    const pixelsColors = getPixelColors(data, pixels);
+    const closestColors = getClosestColor(pixelsColors);
+
+    return countBy(closestColors);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getCountOfColors();
