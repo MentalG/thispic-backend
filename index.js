abe3391160193079205bd2e8b082ded57764780f
MentalG <gromovthefirst@gmail.com>
2020-08-21 02:32:13 +0300

    add: CRUD system for posts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const postsRoutes = require('./src/routes/posts')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const app = express();
 const DB_CONNECTION = process.env.DB_CONNECTION;
@@ -11,6 +12,7 @@ const connectionConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(DB_CONNECTION, connectionConfig, () => console.log('Connected'));
 
 app.use(bodyParser.json())
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('We are on home')
 })
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,23 +3,62 @@ const Post = require('../models/post');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('We are on posts')
-})
+//Get create post
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
 
-router.post('/', (req, res) => {
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description
-    })
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
 
-    post.save()
-    .then((data) => {
-        res.json(data);
-    })
-    .catch(err => {
-        res.json({message: err})
-    })
-})
+//Get all posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Get specific post
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+//Delete post
+router.delete('/:postId', async (req, res) => {
+  try {
+    const post = await Post.remove({ _id: req.params.postId });
+    res.json(post);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+//Update post
+router.patch('/:postId', async (req, res) => {
+  try {
+    const post = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(post);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
 
 module.exports = router;
